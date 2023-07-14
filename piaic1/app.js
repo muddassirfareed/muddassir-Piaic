430e13ae07e684a0ce865646a40dc7c75c3aac79
muddassirfareed <128825798+muddassirfareed@users.noreply.github.com>
2023-07-14 03:52:52 -0700

    Add files via upload

---
+++ piaic1/app.js
@@ -0,0 +1,195 @@
+// task 2
+// var Name:string = ("hello Eric would you like tolearn some python today?");
+// console.log(Name)
+// task 3
+// var personName:string = ("muddassir");
+// console.log(personName.toLowerCase());
+// console.log(personName.toUpperCase());
+// task 4
+// var Quotes:string = ("You guys are the magicians of 21st century");
+// console.log("'"+Quotes+"'");
+// task 5
+// var famous_person:string = ("Sir Zia Khan")
+// var massage:string = (famous_person)
+// console.log(massage)
+// task 6
+// var Name:string = "\tMuddassir\n\tFareed"
+// console.log(Name);
+// task 7
+// var num1:number = 4
+// console.log(num1+4);
+// console.log(num1*2);
+// var num2:number = 16
+// console.log(num2/2);
+// console.log(num2-8);
+// task 8
+// console.log(5+3);
+// console.log(16-8);
+// console.log(16/2);
+// console.log(4*2);
+//  task 9
+// var Enumber:number = 22
+// console.log(Enumber+" is my favorite number");
+// task 10
+// let Name:string = ("hello Eric would you like tolearn some python today?");
+// console.log(Name)
+// Eric is beginner he wants to learn some python
+// task 11
+// let Name:string[] = ["Muddassir","Irfan","Owais","huzaifa"]
+// console.log(Name[0])
+// console.log(Name[1])
+// console.log(Name[2])
+// console.log(Name.pop())
+// task 12
+// let Name:string[] = ["Muddassir","Irfan","Owais","huzaifa"]
+// let massage:string = (" your are the magicians of 21st century")
+// console.log(Name[0]+massage)
+// console.log(Name[1]+massage)
+// console.log(Name[2]+massage)
+// console.log(Name.pop()+massage)
+// task 13
+// let myarray:string[] = ["I would like to own mustang car ","I would like to own helicopter ","I want to buy my own house "] 
+// let Am:string = ("one day inshallah ")
+// console.log(myarray[0]+Am);
+// console.log(myarray[1]+Am);
+// console.log(myarray[2]+Am);
+// task 14
+// var invite1:string[]= ["shahood","ismail","shayan "]
+// var massage2:string=("Ajao aj B.B,Q hai gush apna lana")
+// console.log(invite1[0]+massage2)
+// console.log(invite1[1]+massage2)
+// console.log(invite1[2]+massage2)
+// console.log(invite1.pop()+massage2)
+// task 15
+// var guestlist:string[]=["shahood","shayan","Ibrahim"]
+// console.log("any one else to invite me ?")
+// console.log(guestlist[0])
+// console.log(guestlist[1])
+// console.log(guestlist[2])
+// console.log(guestlist.pop())
+// console.log("ismail thats not fear")
+// console.log(guestlist[0]+" "+guestlist[1]+" you both are still in my list")
+// task 16
+// var bigtable:string[]=["shahood","shayan","Ibrahim","abdullah","shoaib","sohail","waqas"]
+// console.log(bigtable+" Guys i found bigtable for dinner")
+// bigtable.push("yaseen")
+// task 17
+// console.log("Sorry i invite only two people for dinner")
+// var bigtable:string[]=["shahood","shayan","Ibrahim","waqas"]
+// bigtable.pop();
+// console.log(bigtable)
+// var bigtable2:string[]=["Abdullah","shoaib"]
+// console.log(bigtable2+"Sorry we are not invite you")
+// console.log(bigtable+"your are still in our guest list")
+// bigtable.splice(0, bigtable.length);
+// console.log(bigtable);
+// TASK 18
+// var place:string[] =["Tokyo","london","dubai","saudi_arab","Qatar"]
+// console.log(place+"Original printing");
+// console.log(place.sort()+"this is sort printing");
+// console.log(place+"its original printing");
+// console.log(place.reverse()+"this is reverse printing");
+// console.log(place+"this is original printing");
+// task 19
+// var guest:string[]= ["shahood","ismail","shayan "]
+// console.log(guest.length)
+// task 20
+// var city:string[]=["mount_kilimanjaro","karachi","pakistan","urdu"]
+// console.log(city[0]+" Is locate in tanzania africa");
+// console.log(city[1]+ " is my favorite city");
+// console.log(city[2]+ " is my first love");
+// console.log(city[3]+" is my local language");
+// task 21
+// var home:string[]=["door","window","tv","netflix"]
+// console.log("when father comes at home")
+// console.log("son open the "+home[0]) ;
+// console.log("son open the "+ home[1]+ " for some air");
+// console.log("where is remote i want to trun on " + home[2]);
+// console.log("your "+home [3]+" subscription is going to end")
+// task 22
+// i face some error in TASK 18
+// var place:string[] =["Tokyo","london","dubai","saudi_arab","Qatar"]
+// console.log(place+"Original printing");
+// console.log(place.sort()+"this is sort printing");
+// console.log(place+"its original printing");
+// console.log(place.reverse()+"this is reverse printing");
+// console.log(place+"this is original printing");
+// task 23
+// let car:string = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// let bike:string = 'Honada';
+// console.log (bike== 'Honda')
+// var day:string = 'tomorrow is tuesday'
+// console.log(day=='tomorrow is tuesday')
+// let laptop:string = 'Asus'
+// console.log(laptop=='Asus')
+// let mobile:string = 'samsung'
+// console.log(mobile=='samsung')
+// let soft_drink:string= 'pepsi'
+// console.log( soft_drink==='steang')
+// let vehical:string = 'Honda'
+// console.log( vehical == 'suzuki')
+// let day1:string = 'wednesday'
+// console.log(day1 == 'tuesday')
+// let sno:number = 9
+// console.log(sno == 7)
+// task 24
+// let string1: string = "Hello";
+// let string2: string = "World";
+// // Equality test
+// if (string1 === string2) {
+//   console.log("Strings are equal");
+// } else {
+//   console.log("Strings are not equal");
+// }
+// // Inequality test
+// if (string1 !== string2) {
+//   console.log("Strings are not equal");
+// } else {
+//   console.log("Strings are equal");
+// }
+// query two task 24
+// function toLowerCase(input: string): string {
+//     return input.toLowerCase();
+//   }
+//   const testCases: string[] = ["Muddassir", "Irfan", "Aqib", "Shahood"];
+//   testCases.forEach((testCase: string) => console.log(`Original: ${testCase},Lowercase: ${toLowerCase(testCase)}`));
+var a = 10;
+var b = 5;
+// // Equality check
+// if (a==b){
+//     console.log("A is not equal then B")
+// }
+// else{
+//     console.log("A is equal than B")
+// }
+// // in equality
+// if (a!==b){
+//     console.log("A is not equal than b")
+// }
+// else{
+//   console.log("A is equal than B ")
+// }
+// if (a>=b){
+//     console.log("A is greater than B")
+// }
+// else{
+//     console.log("A is not greater than B")
+// }
+// if (a<=b){
+//     console.log("A is less then B")
+// }
+// else{
+//     console.log("A is not less than B")
+// }
+var x = 5;
+var y = 10;
+if (x > 0 || y > 0) {
+    console.log("Both x and y are greater than 0.");
+}
+var B = 5;
+var c = 10;
+if (B > 0 || c > 0) {
+    console.log("Both b and c are greater than 0.");
+}
